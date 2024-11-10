56b7b877cdd71fe2139590180d0c9fc7f3d1062c
Ahmedzxere <167022737+Ahmedzxere@users.noreply.github.com>
2024-11-10 20:53:56 +0200

    Update S.py

--- S.py
+++ S.py
@@ -1,6 +1,9 @@
 import discord
+import joblib
 from discord.ext import commands
 from profanity_check import predict
+from sklearn.externals import joblib
+
 
 blocked_words = ["احا", "كلمة1", "كلمة2"]
 
